16573c026358844ea645e49509b4a7781874abc6
nmkhans <nurmoin05@gmail.com>
2025-06-20 11:42:02 +0600

    database connected and job api created

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 9000;
+
+app.use(cors());
+app.use(express.json());
 
 const uri = process.env.DB_URI;
 
@@ -20,6 +24,18 @@ const server = async () => {
     await client.connect();
 
     const database = client.db("carrier-code");
+    const jobsCollection = database.collection("jobs");
+
+    //? api
+    app.get("/jobs", async (req, res) => {
+      const result = await jobsCollection.find().toArray();
+      
+      res.json({
+        success: true,
+        message: "All jobs data.",
+        data: result,
+      });
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log("database connected successfully.");
@@ -29,6 +45,10 @@ const server = async () => {
 };
 
 server().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
 app.listen(port, () => {
   console.log(`listening to port ${port}`);
